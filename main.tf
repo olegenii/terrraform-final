7d728dde51afb7679fa59a9777b0ba9fc47540d5
Oleg Satalkin <oleg.satalkin@gmail.com>
2022-04-01 08:49:01 +0000

    init: start rebrain-task-tf2 with .tf droplet definition

---
+++ main.tf
@@ -0,0 +1,52 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "2.19.0"
+    }
+  }
+}
+
+provider "digitalocean" {
+  # Configuration options
+  token = var.do_token
+}
+
+# Create a task_name tag
+resource "digitalocean_tag" "task_name" {
+  name = "task_name:devops-terraform-02"
+}
+
+# Create a user_email tag
+resource "digitalocean_tag" "user_email" {
+  name = "user_email:oleg_satalkin_at_gmail.com"
+}
+
+# Create a new SSH key for admin access from file
+resource "digitalocean_ssh_key" "ubuntu_ssh_admin" {
+  name       = "Ubuntu SSH key"
+  #public_key = var.do_droplet_ssh_key
+  public_key = file("/home/ubuntu/.ssh/id_rsa.pub")
+}
+
+# Get SSH key for rebrain access
+data "digitalocean_ssh_key" "ubuntu_ssh_rebrain" {
+  name = "REBRAIN.SSH.PUB.KEY"
+}
+
+// # Create a new SSH key for rebrain access from cli
+// resource "digitalocean_ssh_key" "ubuntu_ssh_rebrain" {
+//   name       = "REBRAIN.SSH.PUB.KEY"
+//   public_key = var.do_droplet_ssh_key
+// }
+
+# Create a new vps Droplet in the fra1 region with tags and ssh keys
+resource "digitalocean_droplet" "vps" {
+  image  = "ubuntu-20-04-x64"
+  name   = "rebrain-devops-tf2"
+  region = "fra1"
+  size   = "s-1vcpu-1gb"
+  disk = 25
+  tags   = [digitalocean_tag.task_name.id, digitalocean_tag.user_email.id]
+  ssh_keys = [digitalocean_ssh_key.ubuntu_ssh_admin.fingerprint, data.digitalocean_ssh_key.ubuntu_ssh_rebrain.id]
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "do_token" {
+  description = "Access token to DO"
+  type        = string
+  #default     = "https://gitlab.rebrainme.com/api/v4"
+}
+
+// variable "do_droplet_ssh_key" {
+//   description = "SSH key to Droplet"
+//   type        = string
+//   #default     = "https://gitlab.rebrainme.com/api/v4"
+// }
